3bde90bb82934a4374573558386661d989c87631
SOLPOC <guojiananabc@gmail.com>
2024-04-24 15:02:30 +0800

    mysa sp

--- MyCycleGAN.py
+++ MyCycleGAN.py
@@ -142,15 +142,71 @@ dm_sample.setup("predict")
 predict_loader = dm_sample.predict_dataloader()
 b__samples = next(iter(predict_loader)) # used to track performance of model during training later
 
-show_img(a_samples, nrow=SAMPLE_SIZE, title="Augmented digital arts")
-show_img(b_samples, nrow=SAMPLE_SIZE, title="Augmented bs")
+# show_img(a_samples, nrow=SAMPLE_SIZE, title="Augmented digital arts")
+# show_img(b_samples, nrow=SAMPLE_SIZE, title="Augmented bs")
 
 """***Lets Define Self Attention Modules***"""
 
 import torch
 import torch.nn.functional as F
 from torch import nn
+class SpectralNorm(nn.Module):
+    def __init__(self, module, name='weight', power_iterations=1):
+        super(SpectralNorm, self).__init__()
+        self.module = module
+        self.name = name
+        self.power_iterations = power_iterations
+        if not self._made_params():
+            self._make_params()
 
+    def _update_u_v(self):
+        u = getattr(self.module, self.name + "_u")
+        v = getattr(self.module, self.name + "_v")
+        w = getattr(self.module, self.name + "_bar")
+
+        height = w.data.shape[0]
+        for _ in range(self.power_iterations):
+            v.data = l2normalize(torch.mv(torch.t(w.view(height,-1).data), u.data))
+            u.data = l2normalize(torch.mv(w.view(height,-1).data, v.data))
+
+        # sigma = torch.dot(u.data, torch.mv(w.view(height,-1).data, v.data))
+        sigma = u.dot(w.view(height, -1).mv(v))
+        setattr(self.module, self.name, w / sigma.expand_as(w))
+
+    def _made_params(self):
+        try:
+            u = getattr(self.module, self.name + "_u")
+            v = getattr(self.module, self.name + "_v")
+            w = getattr(self.module, self.name + "_bar")
+            return True
+        except AttributeError:
+            return False
+
+
+    def _make_params(self):
+        w = getattr(self.module, self.name)
+
+        height = w.data.shape[0]
+        width = w.view(height, -1).data.shape[1]
+
+        u = Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
+        v = Parameter(w.data.new(width).normal_(0, 1), requires_grad=False)
+        u.data = l2normalize(u.data)
+        v.data = l2normalize(v.data)
+        w_bar = Parameter(w.data)
+
+        del self.module._parameters[self.name]
+
+        self.module.register_parameter(self.name + "_u", u)
+        self.module.register_parameter(self.name + "_v", v)
+        self.module.register_parameter(self.name + "_bar", w_bar)
+
+
+    def forward(self, *args):
+        self._update_u_v()
+        return self.module.forward(*args)
+
+from torch.nn import Parameter
 def l2normalize(v, eps=1e-12):
     return v / (v.norm() + eps)
 
@@ -207,7 +263,9 @@ class Downsampling(nn.Module):
             self.block.append(Self_Attn(in_channels,'relu'))
         self.block.append(nn.Conv2d(in_channels,out_channels,kernel_size = kernel_size,stride=stride,padding = padding,bias= not norm))
         if norm:
-            self.block.append(nn.InstanceNorm2d(out_channels,affine = True))
+            # self.block.append(nn.InstanceNorm2d(out_channels,affine = True))
+            self.block[-1]=SpectralNorm(self.block[-1])
+            # self.block[-1]= nn.utils.spectral_norm(self.block[-1])
         self.block.append(nn.LeakyReLU(0.3))
 
 
@@ -228,9 +286,13 @@ class Upsampling(nn.Module):
     ):
         super().__init__()
         self.block = nn.Sequential(
-            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
-                               padding=padding, output_padding=output_padding, bias=False),
-            nn.InstanceNorm2d(out_channels, affine=True),
+            # nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
+            #                    padding=padding, output_padding=output_padding, bias=False),
+            # nn.InstanceNorm2d(out_channels, affine=True),
+            SpectralNorm( nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
+                               padding=padding, output_padding=output_padding, bias=False))
+            # nn.utils.spectral_norm(SpectralNorm(nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
+            #                             padding=padding, output_padding=output_padding, bias=False)))
         )
         if dropout:
             self.block.append(nn.Dropout(0.5))
@@ -349,10 +411,15 @@ class CycleGAN(L.LightningModule):
     def forward(self, z):
         return self.gen_BA(z)
 
+
     def init_weights(self):
         def init_fn(m):
             if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):
-                nn.init.normal_(m.weight, 0.0, 0.02)
+                if hasattr(m, 'weight_bar'):
+                    # 如果是SpectralNorm层，初始化其权重参数
+                    nn.init.normal_(m.weight_bar, 0.0, 0.02)
+                else:
+                    nn.init.normal_(m.weight, 0.0, 0.02)
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0.0)
             elif isinstance(m, nn.InstanceNorm2d):
