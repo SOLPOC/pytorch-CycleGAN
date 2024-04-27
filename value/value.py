f5a480924c0f66289325f60344ed259429c52243
SOLPOC <guojiananabc@gmail.com>
2024-04-27 20:32:17 +0800

    value

--- value/value.py
+++ value/value.py
@@ -7,7 +7,7 @@ import re
 from psnr import get_mean_PSNR
 from ssim import get_mean_SSIM
 
-def split(folder_path):
+def split(folder_path,direction="b2a"):
     """
     将 test.py 程序生成的 result/[model_name] 文件夹下分为
     A_real(test.py输入的测试图片)
@@ -19,9 +19,13 @@ def split(folder_path):
 
     """
     all_files = os.listdir(folder_path)
-    direction="A" if "A" in all_files[0] else "B"
-    folder_real= os.path.join(folder_path,"A_real")
-    folder_fake = os.path.join(folder_path,"B_fake")
+    if direction=="b2a":
+        folder_real= os.path.join(folder_path,"B_real")
+        folder_fake = os.path.join(folder_path,"A_fake")
+    if direction == "a2b":
+        folder_real = os.path.join(folder_path, "A_real")
+        folder_fake = os.path.join(folder_path, "B_fake")
+
     os.makedirs(folder_real, exist_ok=True)
     os.makedirs(folder_fake, exist_ok=True)
     for file_name in all_files:
@@ -95,51 +99,75 @@ def count_files_in_folder(folder_path):
 def crossline():
     print("--------------------------------------------------")
 
-def value(results_dir,datasets_dir,transform=False,direction="both"):
+def value(results_dir,datasets_dir,transform=False,direction="b2a"):
 
     print("Start to value results")
 
 
-    split("../results/"+results_dir+"/test_latest/images") # 分割结果文件夹
+    split("../results/"+results_dir+"/test_latest/images",direction) # 分割结果文件夹
     print("Split test results folder into fake and real folders")
+    if direction=="a2b":
+        if transform: # 处理图像
+            transfrom_and_save(
+                "../datasets/"+datasets_dir+"/testB",
+                "../results/"+results_dir+"/test_latest/images/B_real",
+                count_files_in_folder("../results/"+results_dir+"/test_latest/images/B_fake"))
+        else:
+            source_folder_path ="../datasets/"+datasets_dir+"/testB"
+            destination_folder_path = "../results/"+results_dir+"/test_latest/images/B_real"
+            num_files_to_copy = count_files_in_folder("../results/"+results_dir+"/test_latest/images/B_fake")  # 指定要复制的文件数量
+            copy(source_folder_path, destination_folder_path, num_files_to_copy)
+    if direction=="b2a":
+        if transform: # 处理图像
+            transfrom_and_save(
+                "../datasets/"+datasets_dir+"/testA",
+                "../results/"+results_dir+"/test_latest/images/A_real",
+                count_files_in_folder("../results/"+results_dir+"/test_latest/images/A_fake"))
+        else:
+            source_folder_path ="../datasets/"+datasets_dir+"/testA"
+            destination_folder_path = "../results/"+results_dir+"/test_latest/images/A_real"
+            num_files_to_copy = count_files_in_folder("../results/"+results_dir+"/test_latest/images/A_fake")  # 指定要复制的文件数量
+            copy(source_folder_path, destination_folder_path, num_files_to_copy)
 
-    if transform: # 处理图像
-        transfrom_and_save(
-            "../datasets/"+datasets_dir+"/testB",
-            "../results/"+results_dir+"/test_latest/images/B_real",
-            count_files_in_folder("../results/"+results_dir+"/test_latest/images/B_fake"))
-    else:
-        source_folder_path ="../datasets/"+datasets_dir+"/testB"
-        destination_folder_path = "../results/"+results_dir+"/test_latest/images/B_real"
-        num_files_to_copy = count_files_in_folder("../results/"+results_dir+"/test_latest/images/B_fake")  # 指定要复制的文件数量
-        copy(source_folder_path, destination_folder_path, num_files_to_copy)
-
-    print("Copy and transform real images (domain B)")
+    print("Copy and transform real images")
 
     # 计算FID，PSNR，SSIM
     print("Calculate FID score...")
-    cmd="python -m pytorch_fid ../results/"+\
-          results_dir+\
-          "/test_latest/images/B_real " \
-          "../results/"+ \
-          results_dir + \
-          "/test_latest/images/B_fake" \
-          "  --device cuda:0"
+    if direction=="a2b":
+        cmd="python -m pytorch_fid ../results/"+\
+              results_dir+\
+              "/test_latest/images/B_real " \
+              "../results/"+ \
+              results_dir + \
+              "/test_latest/images/B_fake" \
+              "  --device cuda:0"
+    else:
+        cmd="python -m pytorch_fid ../results/"+\
+              results_dir+\
+              "/test_latest/images/A_real " \
+              "../results/"+ \
+              results_dir + \
+              "/test_latest/images/A_fake" \
+              "  --device cuda:0"
     output_lines = os.popen(cmd).readlines()
     pattern = r"FID:\s*(\d+\.\d+)"
     matches = re.findall(pattern, str(output_lines))
     FID=float(str(matches[0]))
 
-    B_real_dir="../results/"+results_dir+"/test_latest/images/B_real"
-    B_fake_dir="../results/"+results_dir+"/test_latest/images/B_fake"
+    if direction=="a2b":
+        real_dir="../results/"+results_dir+"/test_latest/images/B_real"
+        fake_dir="../results/"+results_dir+"/test_latest/images/B_fake"
+    else:
+        real_dir = "../results/" + results_dir + "/test_latest/images/A_real"
+        fake_dir = "../results/" + results_dir + "/test_latest/images/A_fake"
 
     # PSNR计算方式：B_real和B_fake文件夹随机图片之间计算psnr并取平均值，重复以上过程取平均值
     print("Calculate PSNR score...")
-    PSNR=round(get_mean_PSNR(B_fake_dir,B_real_dir,100),4)
+    PSNR=round(get_mean_PSNR(fake_dir,real_dir,1),4)
 
     # SSIM计算方式：B_real和B_fake文件夹随机图片之间计算ssim并取平均值，重复以上过程取平均值
     print("Calculate SSIM score...")
-    SSIM=get_mean_SSIM(B_fake_dir,B_real_dir,10)
+    SSIM=get_mean_SSIM(fake_dir,real_dir,1)
 
     crossline()
     print("FID : "+str(round(FID,4)))
@@ -160,10 +188,11 @@ if __name__ == "__main__":
     # value("shuimo_resnet","shuimo",transform=True,dircetion="both")
     # value("map_resnet", "maps", transform=True)
     # value("map_unet", "maps", transform=True)
-    # value("painting_resnet_b2a", "vangogh2photo", transform=True)
-    # value("painting_unet_b2a", "vangogh2photo", transform=True)
-    value("painting_unet_sp_b2a", "vangogh2photo", transform=True)
-    # value("painting_saunet_b2a", "vangogh2photo", transform=True)
+    value("painting_resnet_b2a", "vangogh2photo", transform=True,direction="b2a")
+    value("painting_unet_b2a", "vangogh2photo", transform=True,direction="b2a")
+    value("painting_saunet_b2a", "vangogh2photo", transform=True,direction="b2a")
+    # value("painting_unet_sp_b2a", "vangogh2photo", transform=True,direction="b2a")
+
     # value("painting_saunet_old", "vangogh2photo", transform=True)
 
 
